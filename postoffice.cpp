4d97481c50854fb1eda5d0971c7be4142f141873
dzienkiewicz <pzkantor195@gmail.com>
2019-01-02 16:29:47 +0100

    pliki h

--- postoffice.cpp
+++ postoffice.cpp
@@ -3,3 +3,34 @@
 //
 
 #include "postoffice.h"
+std::shared_ptr<IPostOffice> IPostOffice::create(unsigned gate_count) {
+    return std::shared_ptr<IPostOffice>();
+}
+
+Postoffice::Postoffice(unsigned gate_count) {
+
+}
+
+Postoffice::~Postoffice() {
+
+}
+
+std::vector<std::string> Postoffice::getStatus() {
+    return std::vector<std::string>();
+}
+
+std::shared_ptr<IClient> Postoffice::getClient(const std::string &identificationNumber) {
+    return std::shared_ptr<IClient>();
+}
+
+void Postoffice::enqueueClient(const std::shared_ptr<IClient> &client) {
+
+}
+
+void Postoffice::gateReady(unsigned gateIndex) {
+
+}
+
+void Postoffice::collectPackages(unsigned gateIndex) {
+
+}
--- postoffice.h
+++ postoffice.h
@@ -5,8 +5,26 @@
 #ifndef POCZTA_POSTOFFICE_H
 #define POCZTA_POSTOFFICE_H
 #include "ipostoffice.h"
+#include <string>
+#include <fstream>
+#include <iostream>
+#include "client.h"
 
-class postoffice {
+class Postoffice: public IPostOffice {
+public:
+    explicit Postoffice(unsigned gate_count);
+    ~Postoffice() override;
+    std::vector<std::string> getStatus() override;
+    std::shared_ptr<IClient> getClient(const std::string &identificationNumber) override;
+    void enqueueClient(const std::shared_ptr<IClient> &client) override;
+    void gateReady(unsigned gateIndex) override;
+    void collectPackages(unsigned gateIndex) override;
+
+private:
+    unsigned gateCount;
+    std::vector<std::shared_ptr<IClient>> queue;
+    std::vector<std::shared_ptr<IClient>> gate;
+    std::vector<std::shared_ptr<IClient>> clients;
 
 };
 
